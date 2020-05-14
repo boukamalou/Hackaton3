6d916c168e607f2c97a47cb0b051c9a8eca7de2e
Morgane-V <morganevanduch@gmail.com>
2020-05-14 13:35:00 +0200

    requete

--- controller_insertion.php
+++ controller_insertion.php
@@ -1,14 +1,22 @@
 <?php
 
-$recherche = $_POST['id_cas'];
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=covid;charset=utf8', 'stagiaire', 'stagiairealaji');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 
-        $db = new PDO('mysql:host=localhost;dbname=covid','stagiaire','stagiairealaji');
 
-            $sql = "SELECT * FROM cas WHERE id_cas = '$recherche' ";
-            $req = $db->prepare($sql);
-            $result = $req->execute(); 
-            echo $result; 
-            
+$reponse = $bdd->prepare("INSERT INTO * FROM cas WHERE id_cas = '$recherche' ");
+$req->bindParam(':new_cas', PDO::PARAM_INT);
+$req->bindParam(':deces', PDO::PARAM_INT);
+$req->bindParam(':guerison', PDO::PARAM_INT);
+$req->bindParam(':date_k', PDO::PARAM_STR);
+$result = $reponse->execute();
+$donnees = $result->fetchAll();
 
 
 
