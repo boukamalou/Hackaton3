f6a4d68b7e4234260c793ce1242ed96587af74bc
JJonathan <jolivald.jonathan@gmail.com>
2020-05-14 16:02:54 +0200

    imprive line chart

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ $('#formulaire_ajout').submit(function(event) {
   console.log('submitted', this, event);
   event.preventDefault();
   var data = {
-    //id_cas: $('#id_cas').val(),
     new_cas: $('#new_cas').val(),
     deces: $('#deces').val(),
     guerison: $('#guerison').val(),
@@ -27,13 +26,12 @@ $('#formulaire_ajout').submit(function(event) {
 // requête ajax GET quand changement selecteur de pays
 $('#selectionner_pays').change(function() {
   var id_pays = $(this).val();
+  var title = $(this).find(":selected").text();
   $.ajax({
     url: 'ajax.php?id=' + id_pays,
     method: 'GET',
     dataType: 'json',
     success: function(data) {
-      console.log('GET success', data);
-      // TODO créer et afficher le graphique
       var rows = data.map(function(cas) {
         return [
           new Date(cas.date),
@@ -42,50 +40,32 @@ $('#selectionner_pays').change(function() {
           parseInt(cas.guerison)
         ];
       });
-      drawChart(rows);
+      drawChart(title, rows);
     }
   });
 });
 
 
 google.charts.load('current', {'packages': ['line']});
-/*google.charts.setOnLoadCallback(drawChart.bind(null, [
-  [1,  37.8, 80.8, 41.8],
-  [2,  30.9, 69.5, 32.4],
-  [3,  25.4,   57, 25.7],
-  [4,  11.7, 18.8, 10.5],
-  [5,  11.9, 17.6, 10.4],
-  [6,   8.8, 13.6,  7.7],
-  [7,   7.6, 12.3,  9.6],
-  [8,  12.3, 29.2, 10.6],
-  [9,  16.9, 42.9, 14.8],
-  [10, 12.8, 30.9, 11.6],
-  [11,  5.3,  7.9,  4.7],
-  [12,  6.6,  8.4,  5.2],
-  [13,  4.8,  6.3,  3.6],
-  [14,  4.2,  6.2,  3.4]
-]));*/
 
-function drawChart(rows) {
-  var data = new google.visualization.DataTable();
+function drawChart(title, rows) {
+  var data  = new google.visualization.DataTable();
+  var chart = new google.charts.Line($('#line_top_x')[0]);
   data.addColumn('date', 'Date');
   data.addColumn('number', 'Nouveaux cas');
-  data.addColumn('number', 'Cas mortel');
-  data.addColumn('number', 'Cas remis');
+  data.addColumn('number', 'Décès');
+  data.addColumn('number', 'Guérison');
   data.addRows(rows);
-  var options = {
-      chart: {
-        title: 'Nom du pays consulté',
-        //subtitle: 'Nombre de cas'
-      },
-      width: 900,
-      height: 500,
-      axes: {
-        x: {
-            0: { side: 'top' }
-        }
+  chart.draw(data, google.charts.Line.convertOptions({
+    chart: {
+      title: 'Evolution du virus en ' + title
+    },
+    width: 798,
+    height: 600,
+    axes: {
+      x: {
+          0: { side: 'top' }
       }
-  };
-  var chart = new google.charts.Line(document.getElementById('line_top_x'));
-  chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+}));
 }
