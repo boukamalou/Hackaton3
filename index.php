f982eb3aee2f31e9b1b1774514bec65ae8ff19d3
JJonathan <jolivald.jonathan@gmail.com>
2020-05-14 15:36:51 +0200

    improve index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <!doctype html>
 <html lang="en">
     <head>
-        <title>Graphique</title>
+        <title>COVID Live</title>
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -13,11 +13,12 @@
         <link rel="stylesheet" href="style.css" />
     </head>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">COVID Live</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item dropdown">
-                        <select class="dropdown-item" id ="selectionner_pays">
+                        <select class="custom-select" id ="selectionner_pays">
                             <option class="dropdown-item" value="13">Afrique du sud</option>
                             <option class="dropdown-item" value="8">Algérie</option>
                             <option class="dropdown-item" value="4">Allemagne</option>
@@ -41,14 +42,14 @@
             </div>
 
             <div id="id01" class="modal">
-            <form id="formulaire_ajout" class="modal-content" method="post">
+            <form id="formulaire_ajout" class="modal-content p-4" method="post">
                 <div class="container">
                 <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
                 <h1>Mettre à jour les statistiques</h1>
                 <div class="row">
             <div class="col-sm-6">
                 <label for="add_pays">Pays</label>
-                    <select id="add_pays">
+                    <select id="add_pays" class="custom-select">
                         <option value="13">Afrique du sud</option>
                         <option value="8">Algérie</option>
                         <option value="4">Allemagne</option>
@@ -117,7 +118,8 @@
         </nav>
     <body>
                 <div id="line_top_x">
-                    <p>blabla</p>
+                    <!-- 798x600 pixels -->
+                    <img src="798px-World_map_(Miller_cylindrical_projection,_blank).svg.png" alt="Carte du monde" />
                 </div>
 
 
